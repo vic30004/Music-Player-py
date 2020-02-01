407a05af16777a2a2e52526fb2b867e159178ff1
vic30004 <victor_993@msn.com>
2020-02-01 15:46:08 -0800

    created the GUI and added a title for it

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-import Pygame
+import pygame
 import tkinter as tkr
 from tkinter.filedialog import askdirectory
 import os
 
+musicplayer = tkr.Tk()
+musicplayer.title("Music Player")
+musicplayer.geometry("450 X 350")
