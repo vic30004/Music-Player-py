74c06271f264474bae56c57b6768a62b472778cd
vic30004 <victor_993@msn.com>
2020-02-01 15:53:28 -0800

    added a for loop to loop through music

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ musicplayer.title("Music Player")
 musicplayer.geometry("450 X 350")
 
 directory = askdirectory()
-os.chdir(directory)#change the directory to a specified path.
+os.chdir(directory)  # change the directory to a specified path.
 
-songList = os.listd() #returns a list containing a name of the entries in the specified path
-playlist = tkr.Listbox(musicplayer ,font="Helvetica 12 bold",bg="white", selectmode = tkr.SINGLE ) # this will display a list of the songs 
+# returns a list containing a name of the entries in the specified path
+songList = os.listd()
+playlist = tkr.Listbox(musicplayer, font="Helvetica 12 bold", bg="white",
+                       selectmode=tkr.SINGLE)  # this will display a list of the songs
 
+# Each time we loop, we select a song and the position of the song will increase
+for item in songList:
+    position = 0
+    playlist.insert(position, item)
+    position = position + 1
 
-
-
-
+pygame.init()
+pygame.mixer.init()
