d3397a8ad55fe21b25ff9cc3c91877b5266c1674
vic30004 <victor_993@msn.com>
2020-02-01 15:50:55 -0800

    added a section where the songs will be displayed

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ import os
 musicplayer = tkr.Tk()
 musicplayer.title("Music Player")
 musicplayer.geometry("450 X 350")
+
+directory = askdirectory()
+os.chdir(directory)#change the directory to a specified path.
+
+songList = os.listd() #returns a list containing a name of the entries in the specified path
+playlist = tkr.Listbox(musicplayer ,font="Helvetica 12 bold",bg="white", selectmode = tkr.SINGLE ) # this will display a list of the songs 
+
+
+
+
+
